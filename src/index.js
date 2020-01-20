86c5ced1ed6f2dc50b82b45e470e50c3ef977447
Lope Ariyo <lopeariyo@gmail.com>
2020-01-20 10:48:42 +0000

    progress with renderMap function

--- src/index.js
+++ src/index.js
@@ -17,8 +17,22 @@ function findPlayerLocation (){
         console.log("User's current latitude is " + playerCoordinates.latitude + ".");
         console.log("User's current longitude is " + playerCoordinates.longitude + ".");
 
-    })
-    
+        renderMap(playerCoordinates.latitude, playerCoordinates.longitude)
+
+    })   
 }
 
-findPlayerLocation()
+function renderMap (latitude, longitude){
+
+    mapboxgl.accessToken = 'pk.eyJ1IjoibG9wZWFyaXlvIiwiYSI6ImNrNWpkamFrcTAyM2IzZXBja3dncmtld3AifQ.-T1q9Tw23a3tqqJ9CYFllg';
+    
+    var map = new mapboxgl.Map({
+        container: 'map', // container id
+        style: 'mapbox://styles/mapbox/dark-v10', //hosted style id
+        center: [longitude, latitude], // starting position [longitude, latitude], needs to be generated and shown on map 
+        zoom: 15 // starting zoom
+    });
+    console.log(document.querySelector("#map"))
+}
+
+findPlayerLocation() 
