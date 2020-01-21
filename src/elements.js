33454efd87400fc292edbd30217a175b37f54eec
Lope Ariyo <lopeariyo@gmail.com>
2020-01-21 09:21:11 +0000

    there is now a pulsing dot to represent the user

--- src/elements.js
+++ src/elements.js
@@ -1,12 +1,4 @@
-//Page Elements
-// let mapDisplay = document.querySelector("#map")
 
-//Creating Element
-function createElement(tag, options = {}) {
-    const el = document.createElement(tag)
-    if (options.innerText) el.innerText = options.innerText
-    if (options.onClick) el.addEventListener("click", options.onClick)
-    return el
-}
 
-// mapDisplay.append(startButton)
+
+
